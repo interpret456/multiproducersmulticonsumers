e54cbdaeb1ce5aa89e05b0229f6420f20f91b7a4
Antoine Salesse <antoine_salesse@ext.connect-tech.sncf>
2022-11-11 18:48:33 +0100

    first

---
+++ lib/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.gradle.api.JavaVersion.VERSION_17
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+    `java-library`
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.guava:guava:31.0.1-jre")
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+    api("org.apache.commons:commons-math3:3.6.1")
+}
+
+version = "0.1.0"
+group = "fr.tawane.multiproducermulticonsumer"
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+java {
+    withSourcesJar()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = VERSION_17.toString()
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/t4w4n3/multiproducersmulticonsumers")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
---
+++ lib/src/main/kotlin/library/Library.kt
@@ -0,0 +1,7 @@
+package library
+
+class Library {
+    fun someLibraryMethod(): Boolean {
+        return true
+    }
+}
